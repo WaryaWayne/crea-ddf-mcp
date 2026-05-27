0e11c8bd109ee2a52110076d53ececaff91c4799
Abdullahi Mohamed <126521894+WaryaWayne@users.noreply.github.com>
2026-05-26 21:13:52 -0400

    Remove unused Effect database and JSON utility modules

--- src/sdk/runtime.ts
+++
@@ -1,8 +0,0 @@
-import { DdfDatabase } from "crea-ddf/db";
-import { Effect } from "effect";
-
-export const dbOnlyLayer = DdfDatabase.layerConfig;
-
-export const runDdfDatabase = <A, E>(
-  effect: Effect.Effect<A, E, DdfDatabase>,
-) => Effect.runPromise(Effect.provide(effect, dbOnlyLayer));
